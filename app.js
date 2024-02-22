f386c09970af76cf32b3d1d432730fdb5ce9f514
deshinta <deshintaaulia065@gmail.com>
2024-02-22 13:22:16 +0700

    feat: add link deploy client

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { 
   cors: {
-    origin: "http://localhost:5173"
+    origin: "https://tic-tac-tue.web.app"
   }
  });
 
