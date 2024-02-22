e874e563f21da1e654be75338f31debaad49a239
deshinta <deshintaaulia065@gmail.com>
2024-02-22 18:14:35 +0700

    feat: debug

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { 
   cors: {
-    origin: "http://localhost:5173"
+    // origin: "http://localhost:5173"
+    origin: "https://tic-tac-tue.web.app"
   }
  });
 
